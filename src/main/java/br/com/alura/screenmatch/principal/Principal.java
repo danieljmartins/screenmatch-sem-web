17074470efea03a8fd6555e01587e6a687e156b7
Daniel <danieljjmartins@gmail.com>
2024-02-23 22:12:35 -0300

    manipulando colecoes de dados

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,13 +1,14 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -16,8 +17,9 @@ public class Principal {
     private final String API_KEY  = "&apikey=4521711a";
     private ConverteDados conversor = new ConverteDados();
 
-    public void exibeMenu(){
+    public void exibeMenu() {
         try {
+
             // busca a serie pelo nome
             System.out.println("Digite o nome da série para busca: ");
             var nomeSerie = leitura.nextLine();
@@ -36,11 +38,33 @@ public class Principal {
             }
             temporadas.forEach(System.out::println);
 
-            //Exibe o título de cada temporada
+            //Exibe o título de cada episodio
             temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
-        }catch (NullPointerException e){
+
+            // Pega os dados de cada episodio
+            List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                    .flatMap(t -> t.episodios().stream())
+                    .collect(Collectors.toList());
+
+            // Busca os 5 melhores episodios
+            System.out.println("\nTop 5 episodios");
+            dadosEpisodios.stream()
+                    .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                    .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                    .limit(5)
+                    .forEach(System.out::println);
+
+            List<Episodio> episodios = temporadas.stream()
+                    .flatMap(t -> t.episodios().stream()
+                            .map(d -> new Episodio(t.numero(), d))
+                    ).collect(Collectors.toList());
+
+            episodios.forEach(System.out::println);
+
+
+        } catch (NullPointerException e) {
             System.out.println("Título não informado");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Falha ao buscar dados\n" + e);
         }
     }
